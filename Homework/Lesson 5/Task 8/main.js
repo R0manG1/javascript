b80e02834931537419e62536625819ca5c68f6ea
R0manG1 <gromus99@protonmail.com>
2025-09-29 16:43:19 +0300

    JS | HW -5 29.09.2025

--- Homework/Lesson 5/Task 8/main.js
+++ Homework/Lesson 5/Task 8/main.js
@@ -44,3 +44,39 @@ let getMinValue = (array) => {
     return console.log('Min number: ', min);
 }
 getMinValue([23, 56, 12, 76, 89, 10, -5, -15]);
+
+//– створити функцію sum(arr), яка приймає масив чисел, сумує значення елементів масиву та повертає його.
+// Приклад sum([1,2,10]) //->13
+let getArraySum = (arr) => {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+console.log(getArraySum([2, 4, 3, 5]));
+
+//– створити функцію swap(arr,index1,index2). Функція міняє місцями значення у відповідних індексах
+let swap = (arr, index1, index2) => {
+    if (index1 !== index2 && index1 < arr.length && index2 < arr.length && index1 >= 0 && index2 >= 0) {
+        let temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+        return arr;
+    }
+    return 'Error!'
+}
+console.log(swap([2, 5, 6, 9], 1, 3));
+
+//– Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
+// Приклад exchange(10000,[{currency:’USD’,value:40},{currency:’EUR’,value:42}],’USD’) // => 250
+let exchange = (sumUAH, currencyValues, exchangeCurrency) => {
+    console.log('Exchange Result: ');
+    if (exchangeCurrency === 'USD') {
+        return (sumUAH / currencyValues[0].value).toFixed(2);
+    } else if (exchangeCurrency === 'EUR') {
+        return (sumUAH / currencyValues[1].value).toFixed(2);
+    }
+    return 'Error!';
+}
+console.log(exchange(10000, [{currency: 'USD', value: 40}, {currency: 'EUR', value: 42}], 'USD'));
